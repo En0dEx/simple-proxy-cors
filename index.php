f90ee57c9d46b40e45acc25e71676679983d6791
steven b <steven.bartosz@etu.univ-orleans.fr>
2016-03-25 14:48:44 +0100

    Add original content-type

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+// Cors
 $cors_headers = array(
     'Access-Control-Allow-Origin: *',
     'Access-Control-Allow-Methods: POST, GET, OPTIONS',
@@ -8,8 +8,21 @@ foreach ($cors_headers as $cors_header) {
     header($cors_header);
 }
 
+// Curl
 $curl = curl_init();
 curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => true,
     CURLOPT_URL => $_GET['get'],
+    CURLOPT_HEADER => true
 ));
-curl_exec($curl);
+$response = curl_exec($curl);
+list($header, $body) = explode("\r\n\r\n", $response, 2);
+
+$requestContentType = curl_getinfo($curl, CURLINFO_CONTENT_TYPE);
+$contentType = "Content-Type: " . $requestContentType;
+
+header($contentType);
+
+curl_close($curl);
+
+echo $body;
